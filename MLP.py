a94925c98d2013242e5a28db94b985f5ca9902fc
lxy <577086410@qq.com>
2024-05-17 17:03:27 +0800

    finish: data define of MLP

--- MLP.py
+++ MLP.py
@@ -1,16 +1,9 @@
-import torch 
-from torch import nn	
-from torch.utils.data import TensorDataset, DataLoader
 import tqdm as tqdm
 import pandas as pd
-       
+import numpy as np  
 
-def init_weights(m):
-	if type(m) == nn.Linear:
-		nn.init.normal_(tensor=m.weight, mean=0, std=0.01)
-		nn.init.zeros_(tensor=m.bias)
-
-
+# Utils
+# 参考《动手学深度学习》
 def accuracy(y_hat, y):
 	"""计算预测正确的数量"""
 	y_hat = y_hat.argmax(axis=1)
@@ -33,49 +26,58 @@ class Accumulator:
 		return self.data[idx]
 
 
-def evaluate_accuracy(net, data_iter):  #@save
-    """计算在指定数据集上模型的精度"""
-    if isinstance(net, torch.nn.Module):
-        net.eval()  # 将模型设置为评估模式
-    metric = Accumulator(2)  # 正确预测数、预测总数
-    with torch.no_grad():
-        for X, y in data_iter:
-            metric.add(accuracy(net(X), y), y.numel())
-    return metric[0] / metric[1]
+# 参数
+input_size = 784
+hidden_size = 128
+output_size = 10
+number_hidden_layers = 5
 
+# Math functions
+def init_weights(m):
+	pass
 
-def train(net, train_iter, lr, loss):
-	updater = torch.optim.SGD(params=net.parameters(), lr=lr)
-	metric = Accumulator(3)  # 记录总损失，总TP，数据数
-	for epoch in range(num_epochs):
-		# for X, y in tqdm.tqdm(train_iter):
-		for X, y in train_iter:
-			X = X.to(device)
-			y = y.to(device)
-			y_hat = net(X)
-			batch_loss = loss(y_hat, y)
-			updater.zero_grad()
-			batch_loss.mean().backward()
-			updater.step()
-			metric.add(float(batch_loss.sum()), accuracy(y_hat, y), y.numel()/10)
-		aver_loss = metric[0] / metric[2]
-		aver_acc = metric[1] / metric[2]
-		test_acc = aver_acc
-		print("Epoch: %d, Loss: %.4f, Acc: %.4f"%(epoch, aver_loss, aver_acc))
-		# print("%d & %.4f & %.4f \\\\"%(epoch, aver_loss, aver_acc))
-		metric.reset()
+def sigmoid(x):
+	return 1 / (1 + np.exp(-x))
 
+def grad_sigmoid(x):
+	return sigmoid(x) * (1 - sigmoid(x))
 
-def ans(net, eval_iter):
-	with open("mlp-ans.csv", "w") as f:	
-		X = test_input.to(device)
-		y_hat = net(X)
-		y_hat = y_hat.argmax(axis=1)
-		df = pd.DataFrame(columns=['ImageId', 'Label'])
-		df['ImageId'] = range(1, len(y_hat)+1)
-		df['Label'] = y_hat.cpu().numpy()
-		df.to_csv(f, header=True, index=False)
+def softmax(x):
+	return np.exp(x) / np.sum(np.exp(x), axis=1, keepdims=True)
 
+def grad_softmax(x):
+	return (1 - softmax(x)) * softmax(x)
+
+def linear(X, w, b):
+	return np.matmul(X, w) + b
+
+w_input = np.zero([input_size, hidden_size], dtype='f')
+w_hidden_arr = np.zero([number_hidden_layers-1, hidden_size, hidden_size], dtype='f')
+w_output = np.zero([hidden_size, output_size], dtype='f')
+
+b_input = np.zero([hidden_size], dtype='f')
+b_hidden_arr = np.zero([number_hidden_layers, hidden_size], dtype='f')
+b_output = np.zero([output_size], dtype='f')
+
+w_input_grad = np.zero([784, 128], dtype='f')
+w_hidden_grad_arr = np.zero([4, 128], dtype='f')
+w_output_grad = np.zero([128, 10], dtype='f')
+
+b_input_grad = np.zero([128], dtype='f')
+b_hidden_grad_arr = np.zero([4, 128], dtype='f')
+b_output_grad = np.zero([10], dtype='f')
+
+input_layer = np.zero([784], dtype='f')
+hidden_layer = np.array([5, 128], dtype='f') 
+output_layer = np.zero([10], dtype='f')
+
+def feedforward(X):
+	# input layer
+	hidden_layer[0] = sigmoid(linear(input_layer, w_input, b))
+	for i in range(4):
+		hidden_layer[i+1] = sigmoid(linear(hidden[i], w_hidden_arr[i], b_hidden_arr[i]))
+	output_layer = softmax(linear(hidden_layer[4], w_output, b_output))
+	return None
 
 if __name__ == "__main__":
 	# hyperparameters
@@ -104,7 +106,7 @@ if __name__ == "__main__":
 	loss = nn.CrossEntropyLoss(reduction='none')
 	
 	# net = nn.Sequential(nn.Linear(784, 10))
-	net = nn.Sequential(nn.Linear(784, 256), nn.ReLU(), nn.Linear(256, 10))
+	net = nn.Sequential(nn.Linear(784,10))
 	
 	net.apply(fn=init_weights)
 	device = torch.device("cuda" if torch.cuda.is_available() else \
