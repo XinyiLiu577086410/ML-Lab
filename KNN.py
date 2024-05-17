64f8c93097d91f9378614c4e08e13f99cc5f795a
lxy <577086410@qq.com>
2024-05-17 16:39:00 +0800

    feat: Chinese comment in KNN.py

--- KNN.py
+++ KNN.py
@@ -1,39 +1,40 @@
 import numpy as np
-import pandas as pd # type: ignore
-from mpi4py import MPI # type: ignore
-from tqdm import tqdm # type: ignore
+import pandas as pd 
+from mpi4py import MPI 
+from tqdm import tqdm
 
 def Minkovskiy(x, y, p = 2):
 	return np.sum(abs(x - y) ** p) ** (1 / p)
 
-# parameter:
-K = 5 #  how many nearest neighbors
-P = 2 #  parameter of Minkovskiy's distance
-VecLen = 784 #  length of feature vector
+# 参数:
+K = 5 #  邻居个数
+P = 2 #  Minkovskiy 距离的 p 值
+VecLen = 784 #  特征向量长度
 
 if __name__ == "__main__":
-	# MPI setup
+	# MPI 初始化
 	comm = MPI.COMM_WORLD
 	rank = comm.Get_rank()
 	size = comm.Get_size()
 
 
 	train_pd = pd.read_csv("train.csv")
-	train_np = train_pd.values 	# train_np[0]: label + train_np[1:]: feature
+	# train_np[0]: 标签， train_np[1:]: 特征向量
+	train_np = train_pd.values
 
 	total = 0
 	_total = 0
 
 
-	# MPI distribute workloads
+	# 在 MPI 进程间分配数据 
 	if rank == 0:
 		test_pd = pd.read_csv("test.csv")
-		test_np_send = np.array(test_pd.values.copy(), dtype='i')	# test_np[:]: feature
+		test_np_send = np.array(test_pd.values.copy(), dtype='i')	
 		_total = len(test_np_send)
 		if total % size != 0:
 			print("Fatal Error: total % size != 0")
 			comm.Abort()
-		# padding 0 after test dataset
+		# 在最后一行补 0， 使得数据个数能够被 size 整除 
 		total = (_total + size - 1) // size * size  
 		test_np_send.resize([size, total // size, VecLen])
 	else:
@@ -42,24 +43,27 @@ if __name__ == "__main__":
 	total = comm.bcast(total, root=0)
 
 	test_np_recv = np.empty([total // size, VecLen], dtype='i')
-	print("size of test_np_recv:" ,test_np_recv.shape)
 	comm.Scatter(test_np_send, test_np_recv, root = 0)
+	
+	# 类型: python array
 	res_send = []
 
-	with tqdm(total=len(test_np_recv)) as bar:
-		for point in test_np_recv:
-			distances = np.array([[Minkovskiy(point, x[1:]), x[0]] for x in train_np])
-			distances = distances[distances[:, 0].argsort()][:K]
-			# 统计 label 0～9 在邻近点出现了几次
-			tmp = np.array([[i, np.sum(distances[..., 1] == i)] for i in range(0, 10)]) 
-			max_likelihood = tmp[:, 1].argmax() # 找出出现最多的 label
-			res_send.append(max_likelihood)
-			bar.update(1)
+	for point in test_np_recv:
+		distances = np.array([[Minkovskiy(point, x[1:]), x[0]] for x in train_np])
+		distances = distances[distances[:, 0].argsort()][:K]
+		# 统计 label 0～9 在邻近点出现了几次
+		tmp = np.array([[i, np.sum(distances[..., 1] == i)] for i in range(0, 10)]) 
+		# 找出出现最多的 label
+		max_likelihood = tmp[:, 1].argmax() 
+		res_send.append(max_likelihood)
+
+
 	if rank == 0:
 		res_recv = np.empty([total], dtype='i')
 	else:
 		res_recv = None
 
+	# 将结果收集到 rank 0 进程
 	comm.Gather(np.array(res_send, dtype='i'), res_recv, root = 0)
 	
 	if rank == 0:
